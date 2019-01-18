a9494182c765e2a945b69b2443b5e24b2d89cc65
William Robert Simpson <will.simpson85@gmail.com>
2019-01-18 00:16:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
 new_array = []
 new_array << string
 nextarr = []
-new_array.each {|x| nextarr.shift(x) }
+new_array.each {|x| nextarr.unshift(x.reverse) }
 
 puts(nextarr)
 end
