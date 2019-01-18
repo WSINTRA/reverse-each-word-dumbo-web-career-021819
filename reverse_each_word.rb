936063663810e8553236be9f2bbd715a479bad40
William Robert Simpson <will.simpson85@gmail.com>
2019-01-18 00:08:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
 new_array = []
-new_array << string.each do {|x| x.reverse}
+new_array << string.each do {|x| x.reverse} end
 puts(new_array)
 end
 reverse_each_word("Johnny, jack jim, black""tell em")
